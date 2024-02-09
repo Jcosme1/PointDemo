8f19544427d124a3886b5641808c99d34ca3e284
Julian Cosmeu <jcosme1@ggc.edu>
2024-02-09 12:16:30 -0500

    init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point poin1 = new Point(1,3);
+        NamedPoint point2 = new NamedPoint(3,2,"a:");
+        System.out.println(point2);
+
+    }
+}
---
+++ src/NamedPoint.java
@@ -0,0 +1,15 @@
+public class NamedPoint extends Point {
+    private String name;
+    public NamedPoint(double x, double y, String name){
+        super(x,y); //must put super first
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    @Override
+    public String toString(){
+        //return name + super.toString();
+        return name + "(" + getX() + ", " + getY() + ")";
+    }
+}
